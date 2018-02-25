317f42c0eb220129a31322ad51af88494f545876
Rory Clark <rory@rorywebdev.com>
2018-02-25 14:44:30 +0000

    Main material

---
+++ Assets/Scripts/Game/ShapeController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeController : MonoBehaviour {
+
+    Rigidbody m_rigidbody;
+
+    ShapeSpawner m_spawner;
+
+    float m_maxForce = 0, m_maxTorque = 0;
+    float m_minForce = 0, m_minTorque = 0;
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));   
+    }
+
+    public void SetSpawner(ShapeSpawner _spawner)
+    {
+        m_spawner = _spawner;
+    }
+
+    public void Explode()
+    {
+        // Set our positive and negatives
+        int x = Random.Range(0, 2) * 2 - 1, y = Random.Range(0, 2) * 2 - 1, z = Random.Range(0, 2) * 2 - 1;
+
+        // Force changes the direction
+        Vector3 force = new Vector3(Random.Range(m_minForce, m_maxForce)*x, Random.Range(m_minForce, m_maxForce)*y, Random.Range(m_minForce, m_maxForce)*z);
+        m_rigidbody.AddForce(force);
+
+        // Reset the positive and negatives
+        x = Random.Range(0, 2) * 2 - 1;
+        y = Random.Range(0, 2) * 2 - 1;
+        z = Random.Range(0, 2) * 2 - 1;
+
+        // While torque changes the spin/rotation
+        Vector3 torque = new Vector3(Random.Range(m_minTorque, m_maxTorque)*x, Random.Range(m_minTorque, m_maxTorque)*y, Random.Range(m_minTorque, m_minTorque)*z);
+        m_rigidbody.AddTorque(torque);
+    }
+
+    private void FixedUpdate()
+    {
+        // Destroy this if below the plane.
+        // Could use out of bounds of camera view, however this would also destroy those that haven't fallen off and simply in the left and right edges of the screen
+        if(transform.position.y < -10)
+        {
+            KillShape();
+        }
+    }
+
+    public void ChangeForces(float _force)
+    {
+        m_maxForce = _force;
+        m_maxTorque = _force;
+
+        m_minForce /= 3f;
+        m_minTorque /= 3f;
+
+    }
+
+    void KillShape()
+    {
+        m_spawner.RemoveShape(this);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Game/ShapeSpawner.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpawner : MonoBehaviour {
+
+    GameObject m_sphere, m_cube, m_pyramid;
+
+    Transform m_spawnPosition, m_spawnParent;
+
+    List<ShapeController> m_currentShapes = new List<ShapeController>();
+
+    float m_currentForce = 100f;
+
+    // Shows the private field in the inspector
+    [SerializeField]
+    float m_forceMultiplier = 5f;
+
+    // Use setter and getter so we don't have other classes changing the value
+    // These do not appear in the editor but still allow other classes to see the values
+    public int shapeCount { get; private set; }
+
+    // Use awake for finding/setting objects that are either required for this script, or known to be children of the object
+    private void Awake()
+    {
+
+        // If you put all your prefabs within a folder caled Resources you can then load them without having to assign the value in the editor
+        // It uses the path based on the resource path root you create
+        // e.g. if you create a folder structure of Resources/Prefabs/Resources/Cube you could load it from both the full path and by simply calling Cube due to the second Resources folder
+        m_sphere = Resources.Load<GameObject>("Prefabs/Game/Sphere");
+        if (m_sphere == null)
+            Debug.Log("Unable to load sphere prefab");
+
+        m_cube = Resources.Load<GameObject>("Prefabs/Game/Cube");
+        if (m_cube == null)
+            Debug.Log("Unable to load cube prefab");
+
+        m_pyramid = Resources.Load<GameObject>("Prefabs/Game/Pyramid");
+        if (m_pyramid == null)
+            Debug.Log("Unable to load pyramid prefab");
+
+        m_spawnPosition = transform.Find("SpawnPosition");
+        m_spawnParent = transform.Find("SpawnedObjects");
+
+        shapeCount = 0;
+
+        m_currentForce *= m_forceMultiplier;
+    }
+
+    // Use start for objects that are not related to or part of the object
+	void Start ()
+    {
+        
+	}
+
+    private void Update()
+    {
+        // By default Jump is the space bar in the Input Manager
+        // You can force it to be the space button by using
+        // Input.GetKeyUp(KeyCode.Space)
+        if (Input.GetButtonUp("Jump"))
+        {
+            SpawnNewObject();
+        }
+
+        // Here we'll use the E key, by default nothing is bound to it
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            ExplodeShapes();
+        }
+
+    }
+	
+    public void SpawnNewObject()
+    {
+        GameObject chosen;
+        int objectChoice = Random.Range(0, 3);
+        switch (objectChoice)
+        {
+            default:
+            case 0:
+                chosen = m_sphere;
+                break;
+            case 1:
+                chosen = m_cube;
+                break;
+            case 2:
+                chosen = m_pyramid;
+                break;
+        }
+
+        GameObject go = Instantiate(chosen, m_spawnParent);
+        go.transform.position = m_spawnPosition.position;
+
+        ShapeController sc = go.GetComponent<ShapeController>();
+        sc.SetSpawner(this);
+        sc.name = chosen.name + shapeCount.ToString();
+
+        sc.ChangeForces(m_currentForce);
+
+        m_currentShapes.Add(sc);
+
+        shapeCount++;
+    }
+
+    public void ChangeExplodeForce(float _force)
+    {
+        m_currentForce = _force * m_forceMultiplier;
+        foreach(ShapeController sc in m_currentShapes)
+        {
+            sc.ChangeForces(m_currentForce);
+        }
+    }
+
+    public void ExplodeShapes()
+    {
+        // We're simply calling a function so we'll just use a foreach
+        // If we were editing the item in this script we would use a for loop
+        foreach (ShapeController sc in m_currentShapes)
+        {
+            sc.Explode();
+        }
+    }
+
+    // Remove the destroyed shape from the list so we don't try to call it during explode shapes
+    // If we didn't remove it, we would end up with a MissingReferenceException error
+    public void RemoveShape(ShapeController _sc)
+    {
+        m_currentShapes.Remove(_sc);
+    }
+
+
+}
